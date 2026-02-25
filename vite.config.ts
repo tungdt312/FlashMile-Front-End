866a921b95294288e419177ead27937add7022e1
Gnuttttt <dinhthanhtung0312@gmail.com>
2026-02-25 22:06:27 +0700

    initialize library

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from "@tailwindcss/vite";
 import * as path from "node:path";
+import {tanstackRouter} from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+      tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+  }),
       react(),
-      tailwindcss()
+      tailwindcss(),
+
   ],
   resolve: {
     alias: {
